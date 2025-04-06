2af83a4d04d157c1ee6363d4583f61f03b0d2252
morpheus-webdev <wvwvw22@gmail.com>
2025-04-06 13:31:59 +0200

    2025.04.06. feladat

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { RestaurantContextProvider } from './contexts/RestaurantContext.tsx';
 
-createRoot(document.getElementById('root')!).render(
-	<BrowserRouter>
-		<RestaurantContextProvider>
-			<App />
-		</RestaurantContextProvider>
-	</BrowserRouter>
-);
+createRoot(document.getElementById('root')!).render(<App />);
