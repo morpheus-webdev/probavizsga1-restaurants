a876919106d90dc3a81ccdf1b3fa8840de5ea188
morpheus-webdev <wvwvw22@gmail.com>
2025-04-06 13:26:54 +0200

    2025.04.06 feladat

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Restaurants } from './components/Restaurants';
-import { Home } from './components/Home';
 
 function App() {
-	return (
-		<div id='app'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/restaurants' element={<Restaurants />} />
-			</Routes>
-		</div>
-	);
+	return <div id='app'>x</div>;
 }
 
 export default App;
